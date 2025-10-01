bda788db11eea494b05ba03cd052f0d0d019ccf8
L15451 <alvarezjoaquine@gmail.com>
2025-10-01 14:32:03 +0000

    CI: added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    site: 'https://joaquin-alvarez.github.io',
+    base: '/ciudades-limpias',
+    build: {
+        assets: '_astro',
+        inlineStylesheets: 'auto'
+    },
+    vite: {
+        build: {
+            minify: true,
+            cssMinify: true,
+            cssCodeSplit: true,
+        },
+    },
+});
